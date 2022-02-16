8ebf119ce566c5ac167593679048ecfb8ab118cd
Kenji Brameld <kenjibrameld@gmail.com>
2022-02-16 16:07:32 +0900

    add marking segment
    Signed-off-by: Kenji Brameld <kenjibrameld@gmail.com>

---
+++ src/marking_segment.cpp
@@ -0,0 +1,43 @@
+// Copyright 2021 Kenji Brameld
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <QPainter>
+#include <cmath>
+#include "soccer_vision_msgs_layers/marking_segment.hpp"
+#include "confidence.hpp"
+
+namespace soccer_vision_msgs_layers
+{
+
+void MarkingSegment::overlay(
+  QImage & layer,
+  const soccer_vision_msgs::msg::MarkingSegment & msg)
+{
+  QPainter painter(&layer);
+
+  QPen pen(QColor(0, 255, 0, 100));
+  pen.setWidth(3);
+  pen.setCapStyle(Qt::RoundCap);
+  painter.setPen(pen);
+
+  painter.drawLine(msg.start.x, msg.start.y, msg.end.x, msg.end.y);
+}
+
+}  // namespace soccer_vision_msgs_layers
+
+#include "pluginlib/class_list_macros.hpp"
+
+PLUGINLIB_EXPORT_CLASS(
+  soccer_vision_msgs_layers::MarkingSegment,
+  rqt_image_overlay_layer::PluginInterface)
